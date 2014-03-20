21afb8850a6edd0dd07e7bb761997bd1aa49af66
Takumi Endo <t_endo@tq-mbp.local>
2014-03-21 03:29:49 +0900

    add generated files.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_html5_fields do
+  name 'Redmine Html5 Fields plugin'
+  author 'Takumi Endo'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://github.com/tq-jappy/redmine_html5_fields'
+  author_url 'http://github.com/tq-jappy'
+end

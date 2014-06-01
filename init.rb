8bcce5eed0e47021e13c1195187218009f709e0d
Takumi Endo <tq-ed@v7.com>
2014-06-02 00:01:25 +0900

    add first patch

--- init.rb
+++ init.rb
@@ -1,3 +1,6 @@
+require 'redmine'
+require 'html5_fields/projects_helper_patch'
+
 Redmine::Plugin.register :redmine_html5_fields do
   name 'Redmine Html5 Fields plugin'
   author 'Takumi Endo'
@@ -5,4 +8,11 @@ Redmine::Plugin.register :redmine_html5_fields do
   version '0.0.1'
   url 'http://github.com/tq-jappy/redmine_html5_fields'
   author_url 'http://github.com/tq-jappy'
+
+  Rails.configuration.to_prepare do
+    require_dependency 'projects_helper'
+    unless ProjectsHelper.included_modules.include? Html5Fields::ProjectsHelperPatch
+      ProjectsHelper.send(:include, Html5Fields::ProjectsHelperPatch)
+    end
+  end
 end
---
+++ lib/html5_fields/projects_helper_patch.rb
@@ -0,0 +1,6 @@
+require_dependency 'projects_helper'
+
+module Html5Fields
+  module ProjectsHelperPatch
+  end
+end

0621139c5da7623924bfe493bcf6412112a3a221
Takumi Endo <tq-ed@v7.com>
2014-06-08 22:19:20 +0900

    add range format

--- lib/html5_fields/field_format.rb
+++ lib/html5_fields/field_format.rb
@@ -1,5 +1,42 @@
 module HtmlFields
   module FieldFormat
+    class RangeFormat < Redmine::FieldFormat::Numeric
+      add 'range'
+
+      def label
+        "label_range"
+      end
+
+      def cast_single_value(custom_field, value, customized=nil)
+        value.to_i
+      end
+
+      def validate_single_value(custom_field, value, customized=nil)
+        # FIXME
+        # errs = super
+        # errs << ::I18n.t('activerecord.errors.messages.not_a_number') unless value =~ /^[+-]?\d+$/
+        # errs
+        []
+      end
+
+      def edit_tag(view, tag_id, tag_name, custom_value, options={})
+        view.text_field_tag(tag_name, custom_value.value, options.merge(:id => tag_id, :type => "range"))
+      end
+
+      def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options={})
+        view.text_field_tag(tag_name, value, options.merge(:id => tag_id, :type => "range")) +
+          bulk_clear_tag(view, tag_id, tag_name, custom_field, value)
+      end
+
+      def query_filter_options(custom_field, query)
+        {:type => :integer}
+      end
+
+      def group_statement(custom_field)
+        order_statement(custom_field)
+      end
+    end
+
     class ColorFormat < Redmine::FieldFormat::Unbounded
       add 'color'
       self.searchable_supported = true

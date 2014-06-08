de92bcdf5377eddb659e6752bf1e1b5d3da84d2b
Takumi Endo <tq-ed@v7.com>
2014-06-08 22:22:40 +0900

    refactor

--- lib/html5_fields/field_format.rb
+++ lib/html5_fields/field_format.rb
@@ -61,7 +61,7 @@ module Html5Fields
 
       def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options={})
         value = value.blank? ? "#009900" : value
-        view.text_field_tag(tag_name, value || "#009900", options.merge(:id => tag_id, :size => 10, :type => "color"))
+        view.text_field_tag(tag_name, value, options.merge(:id => tag_id, :size => 10, :type => "color"))
       end
 
       def query_filter_options(custom_field, query)

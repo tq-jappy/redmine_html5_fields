7e863cabbf882d059060897255e3de8c0f65f76e
Takumi Endo <tq-ed@v7.com>
2014-08-26 01:32:24 +0900

    Set requires_redmine version.

--- init.rb
+++ init.rb
@@ -1,6 +1,11 @@
 require 'redmine'
 # require 'html5_fields/projects_helper_patch'
-require 'html5_fields/field_format'
+
+if Redmine::VERSION.to_s >= "2.5"
+  require 'html5_fields/field_format'
+else
+  # FIXME: supports for Redmine 2.4 or lower
+end
 
 Redmine::Plugin.register :redmine_html5_fields do
   name 'Redmine Html5 Fields plugin'
@@ -9,6 +14,8 @@ Redmine::Plugin.register :redmine_html5_fields do
   version '0.0.1'
   url 'http://github.com/tq-jappy/redmine_html5_fields'
   author_url 'http://github.com/tq-jappy'
+
+  requires_redmine :version_or_higher => '2.5'
 
   Rails.configuration.to_prepare do
     # require_dependency 'projects_helper'
